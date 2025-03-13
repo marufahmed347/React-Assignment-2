7ad65546c9861858a4cb356c737524d8109cdd00
marufahmed347 <marufahmed550741@gmail.com>
2025-03-13 19:26:18 +0600

    incidents 1 done

--- src/components/all-pages/incidents/Incidents.jsx
+++ src/components/all-pages/incidents/Incidents.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Card from '../../common/card';
+import IncidentsNavbar from './IncidentsNavbar';
 
 function Incidents() {
   return (
     <>
-    
+    <IncidentsNavbar/>
+    <Card/>
     </>
   )
 }
